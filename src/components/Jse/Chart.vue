8caaa8a477b1d1666fcda0cc9bd73d904640f9ff
Boris Borisov <djslinger77@gmail.com>
2019-04-25 15:53:42 -0400

    Realtime version ready

--- src/components/Jse/Chart.vue
+++ src/components/Jse/Chart.vue
@@ -2,7 +2,6 @@
 <template>
     <div id="app" style="border: 1px solid red">
         <div id="container" style="width:100%; height:75vh;"></div>
-
         <div class="col">
         <div class="card h-100">
             <div class="card-header"><span style="font-size:140%">Quotes</span>
@@ -19,10 +18,8 @@
             </div>
         </div>
         </div>
-
     </div>
-
-
+    
 </template>
 <script>
   import Pusher from 'pusher-js' // https://www.npmjs.com/package/pusher-js
@@ -39,7 +36,7 @@
       var Highchart = require('highcharts/highstock');
       this.chart = Highchart.stockChart('container', Opt.data().options);
       this.HistoryBarsLoad();
-      this.ListenWebSocket(this);
+      this.ListenWebSocket();
     },
     methods: {
       HistoryBarsLoad () {
@@ -54,6 +51,31 @@
             alert("Chart.vue can not get history bars. " + err);
           })
       },
+      ChartBarsUpdate (payload) {
+        let last = this.chart.series[0].data[this.chart.series[0].data.length - 1];
+        // Update the chart only when the series is loaded. WS events can start coming earlier than the chart is loaded.
+        if (last != null) {
+          last.update({
+            // 'open': is created when new bar is added to the chart
+            'high': payload.payload.tradeBarHigh,
+            'low': payload.payload.tradeBarLow,
+            'close': payload.payload.tradePrice
+          }, true);
+        }
+        // New bar is issued. Flag sent from CandleMaker.php
+        if (payload.payload.flag) {
+          // Add bar to the chart. We add just a bar (an empty bar) where all OLHC values are the same. Later these values are gonna update via websocket listener
+          this.chart.series[0].addPoint([
+            payload.payload.tradeDate,
+            payload.payload.tradePrice,
+            payload.payload.tradePrice,
+            payload.payload.tradePrice,
+            payload.payload.tradePrice
+          ], true, false);
+          // Add price channel calculated values. Price channel is calculated on each new bar issued. CandleMaker.php line 165
+          this.HistoryBarsLoad();
+        }
+      },
       ListenWebSocket () {
         this.pusher = new Pusher('c904be2b46a9939a2402', { // Pusher key
           encrypted: true,
@@ -65,14 +87,15 @@
         this.channel.bind("App\\Events\\jseevent", function (data) { // Full event name as shown at pusher debug console
           // Here access to different bot clones will be performed. We have only one ID for now.
           if (data.payload['clientId'] == 12345) {
-            if (data.payload.messageType === 'symbolTickPriceResponse') this.ChartBarsUpdate(e, chart1); // Sent from CandleMaker.php
-            if (data.payload.messageType === 'error') swal("Failed!", "Error: " + e.update['payload'], "warning");
-            if (data.payload.messageType === 'info') toast({type: 'success', title: e.update['payload']});
+            if (data.payload.messageType === 'symbolTickPriceResponse') self.ChartBarsUpdate(data.payload);
+
+            // if (data.payload.messageType === 'error') swal("Failed!", "Error: " + e.update['payload'], "warning");
+            // if (data.payload.messageType === 'info') toast({type: 'success', title: e.update['payload']});
+
             if (data.payload.messageType === 'reloadChartAfterHistoryLoaded') {
               // Vue.toasted.show("Chart is reloaded!", { type: 'success' });
               self.HistoryBarsLoad()
             }
-            ;
           }
         })
       }
