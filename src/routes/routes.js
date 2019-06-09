62158065035b70efcc238b8599453bdc3f0fa7c4
Anastasia Art <art-nastya@inbox.ru>
2019-06-09 04:44:40 +0300

    created dashboard

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,15 +9,10 @@ import Exchnages from 'src/components/Jse/Exchanges.vue'
 import Accounts from 'src/components/Jse/Accounts.vue'
 import Symbols from 'src/components/Jse/Symbols.vue'
 import Bots from 'src/components/Jse/Bots.vue'
-<<<<<<< HEAD
-// import DashboardJseCard from 'src/components/Jse/DashboardJseCard.vue'
-
-=======
 import Strategies from 'src/components/Jse/Trading/Strategies.vue'
 import QueWorker from 'src/components/Jse/Trading/QueWorker.vue'
 import Orders from 'src/components/Jse/Trading/Orders.vue'
 import Trades from 'src/components/Jse/Trading/Trades.vue'
->>>>>>> 75b634af481018a7fccea2ff282cdb30ba9e99c8
 
 export const routes = [
   {
