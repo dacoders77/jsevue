006c1ca70e824e2ce311069a9a3166d9c34698a8
Anastasia Art <art-nastya@inbox.ru>
2019-06-15 12:55:41 +0300

    upgrade load

--- src/components/Jse/DashboardJseCard.vue
+++ src/components/Jse/DashboardJseCard.vue
@@ -1,68 +1,62 @@
 <template>
   <div>
-      <div class="card card-stats card-dashboard p-10">
-        <div class="d-flex">
-          <div >
-            <div class="icon-success">
-              <i class="ti-wallet card-dashboard__icon"></i>
-            </div>
-          </div>
-          <div class="numbers">
-            <p>{{bots[bot -1].name}}</p>
-            <p class="revenue">{{ netProfit }}</p>
+    <div class="card card-stats card-dashboard p-10">
+      <div class="d-flex">
+        <div>
+          <div class="icon-success">
+            <i class="ti-wallet card-dashboard__icon"></i>
           </div>
         </div>
-        <div class="card-dashboard__bar">
-
-          <h5> <!-- account.bots_id == bot.id -->
-            <span v-for="account in accounts">
-            <span v-if="account.id == bots[bot-1].id">
-              <!-- Now go through all exchanges -->
-              <span v-for="exchange in exchanges" v-if="account.exchange_id == exchange.id">{{ exchange.name }}</span>
-            </span>
-            </span>
-          </h5>
-
-          <p class="card-dashboard__status" v-if="bots[bot -1].status == 'idle'">
-            {{bots[bot -1].status}}
-            <i class="card-dashboard__status-icon"></i>
-          </p>
-          <p class="card-dashboard__status" v-else-if="bots[bot -1].status == 'running'">
-            {{bots[bot -1].status}}
-            <i class="card-dashboard__status-icon card-dashboard__status-icon--fill"></i>
-          </p>
-          <p class="card-dashboard__status" v-else>
-            {{bots[bot -1].status}}
-          </p>
-        </div>
-        <div class="card-dashboard__text d-flex flex-grow">
-          <p>
-            <span class="pr-15 card-dashboard__trades" v-if="bots[bot-1].status == 'running'">Trades:
-              <span>{{ total }}</span>
-            </span>
-            <span class="pr-15 card-dashboard__trades" v-else >Trades:</span>
-            <span v-for="symbol in symbols" v-if="symbol.id == bots[bot-1].symbol_id" class="card-dashboard__symbol">{{ symbol.execution_symbol_name }}</span>
-          </p>
-
-          <p v-for="strategy in strategies" v-if="strategy.id == bots[bot-1].strategy_id">{{ strategy.name }}</p>
+        <div class="numbers">
+          <p>{{bot.name}}</p>
+          <p class="revenue">{{ netProfit }}</p>
         </div>
       </div>
+      <div class="card-dashboard__bar">
+
+        <h5> <!-- account.bots_id == bot.id -->
+          <!-- Now go through all exchanges -->
+          <span>{{ exchange.name }}</span>
+        </h5>
+
+        <p class="card-dashboard__status" v-if="bot.status == 'idle'">
+          {{bot.status}}
+          <i class="card-dashboard__status-icon"></i>
+        </p>
+        <p class="card-dashboard__status" v-else-if="bot.status == 'running'">
+          {{bot.status}}
+          <i class="card-dashboard__status-icon card-dashboard__status-icon--fill"></i>
+        </p>
+        <p class="card-dashboard__status" v-else>
+          {{bot.status}}
+        </p>
+      </div>
+      <div class="card-dashboard__text d-flex flex-grow">
+        <p>
+            <span class="pr-15 card-dashboard__trades" v-if="bot.status == 'running'">Trades:
+              <span>{{ total }}</span>
+            </span>
+          <span class="pr-15 card-dashboard__trades" v-else>Trades:</span>
+          <span class="card-dashboard__symbol">{{ symbol.execution_symbol_name }}</span>
+        </p>
+
+        <p>{{ strategy.name }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
   export default {
     props: {
-     bot: Number
+      bot: Object,
+      symbol: Object,
+      strategy: Object,
+      exchange: Object,
+      account: Object
     },
     data() {
       return {
-        bots: [],
-        strategies: null,
-        exchanges: null,
-        symbols: null,
-        accounts: null,
-        botId: '1',
         trades: []
       }
     },
@@ -73,47 +67,33 @@
         notNullTrades = (this.trades.filter(function (trade) {
           return trade.trade_date !== null;
         }));
-        if (typeof(notNullTrades[notNullTrades.length - 1]) !== 'undefined')
+        if (typeof (notNullTrades[notNullTrades.length - 1]) !== 'undefined') {
           return notNullTrades.length;
+        }
       },
       // Revenue
       netProfit: function () {
-        if (typeof(this.trades[this.trades.length - 2]) !== 'undefined')
-          return (this.trades[this.trades.length - 2].net_profit); // Get the penultimate row. Net profit in the last on is always null
-      },
-      // strategy() {
-      //   return this.strategies.find(s => s.id == this.bots[bot-1].strategy_id)
-      // },
-      // account() {
-      //   return this.accounts.find(a => a.id == this.bots[bot-1].id)
-      // },
-      // exchange() {
-      //   return this.exchanges.find(e => e.id == this.account.exchange_id)
-      // }
+        if (typeof (this.trades[this.trades.length - 2]) !== 'undefined') {
+          return (this.trades[this.trades.length - 2].net_profit);
+        } // Get the penultimate row. Net profit in the last on is always null
+      }
     },
-      mounted () {
-        this.loadResources();
-        this.HistoryBarsLoad(this.item);
-      },
-      methods: {
-          loadResources: function () {
-            axios.get('/bot').then(({data}) => (this.bots = data.data));
-            axios.get('/account').then(({data}) => (this.accounts = data.data));
-            axios.get('/exchange').then(({data}) => (this.exchanges = data.data));
-            axios.get('/symbol').then(({data}) => (this.symbols = data.data));
-            axios.get('/strategy').then(({data}) => (this.strategies = data.data));
-          },
-          HistoryBarsLoad(botId) {
-            axios.get('trading/history/' + this.bot) // Back end bot id
-              .then((response) => {
-                this.trades = response.data.rawTable;
-              })
-              .catch((err) => {
-                //alert('trading/history load error');
-                console.log('trading/history load error. DashboardJseCard.vue');
-              })
-          }
-        }
+    mounted() {
+      this.HistoryBarsLoad();
+    },
+    methods: {
+
+      HistoryBarsLoad() {
+        axios.get('trading/history/' + this.bot.id) // Back end bot id
+          .then((response) => {
+            this.trades = response.data.rawTable;
+          })
+          .catch((err) => {
+            //alert('trading/history load error');
+            console.log('trading/history load error. DashboardJseCard.vue');
+          })
+      }
+    }
   }
 
 </script>
--- src/components/Jse/DashboardJseCards.vue
+++ src/components/Jse/DashboardJseCards.vue
@@ -1,31 +1,84 @@
 // DashboardJseCards.vue
 <template>
   <div class="row card-dashboard-wrapper">
-    <div class="col-sm-6 col-lg-3 mb-15" v-for="item in count">
+    <div class="col-sm-6 col-lg-3 mb-15" v-for="bot in bots">
       <!-- 1,2,3,4 -->
       <!-- В DashBoardJseCar получаешь этот irem и ставишшь его в
           url запроса. trading/history/item-->
-      <DashboardJseCard :bot="item" class="h-100"/>
+      <DashboardJseCard :bot="bot"
+                        :account="findAccount(bot)"
+                        :symbol="findSymbol(bot)"
+                        :exchange="findExchange(bot)"
+                        :strategy="findStrategy(bot)"
+                        class="h-100"/>
     </div>
+
   </div>
 </template>
 
 <script>
   import DashboardJseCard from './DashboardJseCard'
+
   export default {
     components: {
       DashboardJseCard
     },
     data: function () {
       return {
-        bots: [],
+        loading: false,
+        strategies: [],
         exchanges: [],
+        symbols: [],
         accounts: [],
-        count: 4,
+        bots: [],
+      }
+    },
+    mounted() {
+      this.loadResources()
+      this.loadBots()
+    },
+    methods: {
+      async loadBots() {
+
+      },
+      async loadResources() {
+
+        this.loading = true
+        try {
+          const resp = await axios.get('/account')
+          this.accounts = resp.data.data
+
+          const respE = await axios.get('/exchange')
+          this.exchanges = respE.data.data
+
+          const respS = await axios.get('/symbol')
+          this.symbols = respS.data.data
+
+          const respSt = await axios.get('/strategy')
+          this.strategies = respSt.data.data
+
+          const respB = await axios.get('/bot')
+          this.bots = respB.data.data
+        } catch (e) {
+
+        }
+        this.loading = false
+      },
+      findSymbol(bot) {
+        return this.symbols.find(s => s.id == bot.symbol_id)
+      },
+      findStrategy(bot) {
+        return this.strategies.find(s => s.id == bot.strategy_id)
+      },
+      findAccount(bot) {
+        return this.accounts.find(a => a.id == bot.id)
+      },
+      findExchange(bot) {
+        return this.exchanges.find(e => e.id == this.findAccount(bot).exchange_id)
       }
     }
   }
-  </script>
+</script>
 <style>
 </style>
 
