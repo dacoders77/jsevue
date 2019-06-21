0fde5f3665b0d632cfa39bf85f20f2bc293558d8
Anastasia Art <art-nastya@inbox.ru>
2019-06-21 23:32:07 +0300

    changes in Log

---
+++ src/components/Jse/QueModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-modal id="modal-json" title="Payload"
+             no-fade
+             data-backdrop="static"
+             keyboard="false"
+             size="lg">
+      <p class="my-4"> {{job.payload}} </p>
+      <tree-view
+        :data="jsonModalMessage"
+        :options="{
+        maxDepth: 5,
+        modifiable: false,
+        link: false}">
+
+      </tree-view>
+    </b-modal>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      job: Object,
+      jsonModalMessage: Object
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
+<style>
+
+</style>
+
