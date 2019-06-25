d260e583d00923ba8f05bd6d27ecb045f07c37f8
Anastasia Art <art-nastya@inbox.ru>
2019-06-25 11:37:40 +0300

    merge

--- src/components/Jse/Que.vue
+++ src/components/Jse/Que.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="row">
       <div class="col-md-12 pb-10">
-        <button type="button" class="btn btn-wd btn-warning btn-fill btn-magnify" @click="truncateQue()">
+        <button type="button" class="btn btn-wd btn-warning btn-fill btn-magnify mr-15" @click="truncateQue()">
                 <span class="btn-label">
                     <i class="ti-search pr-5"></i>
                 </span>Truncate que
@@ -12,22 +12,23 @@
                     <i class="ti-reload pr-5"></i>
                 </span>Reload table
         </button>
-          </div>
-        </div>
+      </div>
+    </div>
     <div class="row mb-30">
       <div class="col-md-12">
 
-        <div class="card">
+        <div class="card card-que">
           <div class="card-header" style="border: 0px solid red; padding: 0px">
             <!--<h4 class="title">Trades log</h4>-->
           </div>
           <div class="card-content table-responsive table-full-width" style="border: 0px solid blue">
             <div class="card-body table-responsive p-0">
-              <table class="table table-hover table-info">
+              <table class="table table-hover table-info" id="table-que">
                 <tbody>
                 <tr>
                   <th><i class="ti-info-alt"></i></th>
                   <th>Queue</th>
+                  <th>Bot ID/Symbol</th>
                   <th>Payload</th>
                   <th>Attempts</th>
                   <th>Reserverd at</th>
@@ -35,19 +36,27 @@
                   <th>Created at</th>
                 </tr>
                 <tr v-for="job in jobs" :key="job.id">
-                  <td>{{ job.id }}</td>
+                  <td> {{ job.id }}</td>
                   <td>{{ job.queue }}</td>
+                  <td>{{ job.data.botSettings.botTitle }} / {{ job.data.botSettings.historySymbolName }}</td>
                   <td>
-                    <button type="button" class="btn btn-fill btn-warning btn-circle" @click="newModalJsonTree(job.payload)">
+                    <button type="button" class="btn btn-fill btn-warning btn-circle"
+                            @click="newModalJsonTree(job)">
                       <i class="ti-server"></i>
                     </button>
-                    <QueModal :jsonModalMessage="jsonModalMessage"  :job="job"/>
 
                   </td>
-                  <td>{{ job.attempts }}</td>
-                  <td>{{ job.reserved_at }}</td>
-                  <td>{{ job.available_at }}</td>
-                  <td>{{ job.created_at }}</td>
+                  <td >{{ job.attempts }}</td>
+                  <td >
+                    <span v-if="job.reserved_at"> {{ job.reserved_at | fullDate }}</span>
+                    <span v-else></span>
+                  </td>
+                  <td><span v-if="job.available_at">{{ job.available_at | fullDate  }}</span>
+                    <span v-else></span>
+                  </td>
+                  <td><span v-if="job.created_at">{{ job.created_at | fullDate  }}</span>
+                    <span v-else></span>
+                  </td>
                 </tr>
                 </tbody>
               </table>
@@ -56,77 +65,83 @@
         </div>
       </div>
     </div>
-
+    <QueModal :jsonModalMessage="jsonModalMessage"/>
   </div>
 </template>
 <script>
   import Vue from 'vue'
   import {ModalPlugin} from 'bootstrap-vue'
-  Vue.use (ModalPlugin)
   import QueModal from './QueModal'
+  Vue.use(ModalPlugin)
 
-    export default {
-      components: {
-        QueModal
+  export default {
+    components: {
+      QueModal
+    },
+    data() {
+      return {
+        form: new Form(),
+        jobs: [],
+        jsonModalMessage: null,
+        perPage: 3,
+        currentPage: 1
+      }
+    },
+    created() {
+      //
+    },
+    mounted() {
+      this.loadResources();
+    },
+    methods: {
+      async loadResources() {
+
+        this.loading = true
+        try {
+          const resp = await axios.get('/job');
+          console.log(resp.data);
+          this.jobs = resp.data
+
+        } catch (e) {}
+        this.loading = false
       },
-      data() {
-        return {
-            form: new Form(),
-            jobs: [],
-            type: ['', 'info', 'success', 'warning', 'danger'], // For notifications
-            notifications: {
-            topCenter: false,
-            jsonModalMessage: []
-          },
-        }
+      truncateQue() {
+        this.form.delete('/job/1') // /job/1, 1 - is not need. Otherwise delete method is not accepted
+          .then((response) => {
+            //Fire.$emit('AfterCreate');
+            this.loadResources();
+            this.showNotification('bottom', 'right', 'Que successfully truncated! <br>' + '&nbsp')
+          })
+          .catch(error => {
+            this.showNotification('bottom', 'right', 'Que truncate error! <br>' + '&nbsp')
+          })
       },
-      created() {
-        //
-      },
-      mounted() {
+
+      reloadTable() {
+        this.jobs = [];
         this.loadResources();
       },
-      methods: {
-        loadResources() {
-          axios.get('/job').then(({data}) => (this.jobs = data.data)); //
-        },
-        truncateQue(){
-          this.form.delete('/job/1') // /job/1, 1 - is not need. Otherwise delete method is not accepted
-            .then((response) => {
-              //Fire.$emit('AfterCreate');
-              this.loadResources();
-              this.showNotification('bottom', 'right', 'Que successfully truncated! <br>' + '&nbsp')
-            })
-            .catch(error => {
-              this.showNotification('bottom', 'right', 'Que truncate error! <br>' + '&nbsp')
-            })
-        },
 
-        reloadTable() {
+      newModalJsonTree(message) {
+        console.log(message);
+        this.jsonModalMessage = message;
+        this.$bvModal.show('modal-json')
 
-        },
+      },
 
-        newModalJsonTree(message) {
-          this.jsonModalMessage = JSON.parse(message);
-          console.log(this.jsonModalMessage);
-          console.log(message);
-          this.$bvModal.show('modal-json')
-
-        },
-
-        showNotification (verticalAlign, horizontalAlign, notificationText) {
-          var color = Math.floor((Math.random() * 4) + 1)
-          this.$notify(
-            {
-              component: {
-                template: "<span>" + notificationText + "</span>"
-              },
-              icon: 'ti-info-alt',
-              horizontalAlign: horizontalAlign,
-              verticalAlign: verticalAlign,
-              type: this.type[color]
-            })
-        },
+      showNotification(verticalAlign, horizontalAlign, notificationText) {
+        var color = Math.floor((Math.random() * 4) + 1)
+        this.$notify(
+          {
+            component: {
+              template: "<span>" + notificationText + "</span>"
+            },
+            icon: 'ti-info-alt',
+            horizontalAlign: horizontalAlign,
+            verticalAlign: verticalAlign,
+            type: this.type[color]
+          })
       }
     }
+  }
 </script>
