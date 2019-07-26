8e4b75d91d426dbf450bb3a54409292b6610433d
Anastasia Art <art-nastya@inbox.ru>
2019-07-27 00:12:47 +0300

    update project

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://45.76.210.96:9603/api"',
+  ROOT_API: '"http://45.76.210.9601/api"',
   LOG_VIEWER_URL: '"http://45.76.210.96:9602/log-viewer/logs"'
 }
