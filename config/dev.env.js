086ba4f98cd066e4bb46c24253f44342aabfb691
Anastasia Art <art-nastya@inbox.ru>
2019-08-07 00:51:58 +0300

    merge to dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // ROOT_API: '"http://45.76.210.96:9603/api"',
+  ROOT_API: '"http://45.76.210.96:9603/api"',
   LOG_VIEWER_URL: '"http://45.76.210.96:9602/log-viewer/logs"',
-  ROOT_API: '"http://jse.kk/api"'
+  // ROOT_API: '"http://jse.kk/api"'
 })
