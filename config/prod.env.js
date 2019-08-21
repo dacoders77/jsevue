fb5f38daf25578a3d0a3a0fe7c0aaca4c5473e78
Anastasia Art <art-nastya@inbox.ru>
2019-08-21 22:35:24 +0300

    add variables in prod

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   ROOT_API: '"http://45.76.210.96:9602/api"',
-  LOG_VIEWER_URL: '"http://45.76.210.96:9602/log-viewer/logs"'
+  LOG_VIEWER_URL: '"http://45.76.210.96:9602/log-viewer/logs"',
+  PUSHER_KEY: '"957f41d7acfeba2194c8"', // Pusher key
+  PUSHER_CLUSTER: ' "ap1" '
 }
